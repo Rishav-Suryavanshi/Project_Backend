a4100ca95eea3d5eee5e05102e9fbf0d1227a45a
RISHAV SURYAVANSHI <rishavkumarsingh.12b1.53@gmail.com>
2024-07-25 10:33:34 +0530

    Project Backend

---
+++ src/config.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const connect = mongoose.connect("mongodb+srv://backend_project:Rishav02@cluster0.qcelsay.mongodb.net/USERS?retryWrites=true&w=majority&appName=Cluster0");
+
+// Check database connected or not
+connect.then(() => {
+    console.log("Database Connected Successfully");
+})
+.catch(() => {
+    console.log("Database cannot be Connected");
+})
+
+// Create Schema
+const Loginschema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// collection part
+const collection = new mongoose.model("USERS", Loginschema);
+
+module.exports = collection;
